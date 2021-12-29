ddbaa8213ffdf64e0aeddcf7c7407cfb0e90248d
echoshihab <echoshihab@gmail.com>
2021-12-29 13:54:24 -0500

    recreate friend contact list using vue cli

---
+++ vue-cli-01/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<section>
+<h2>My Friends</h2>
+<ul>
+    <friend-contact></friend-contact>
+</ul>
+</section>
+</template>
+
+<script>
+
+    export default{
+        data(){
+            return{
+                friends: [
+                    {
+                        id: "Ava",
+                        name: "Ava Adams",
+                        phone: '111 111 1111',
+                        email: 'ava@test.com'
+                    },
+                                    {
+                        id: "Pam",
+                        name: "Pam Beatie",
+                        phone: '222 222 2222',
+                        email: 'ava@test.com'
+                    }
+                    
+                ]
+            };
+        }
+};
+
+
+</script>
---
+++ vue-cli-01/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+<li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong>{{ friend.phone }}</li> 
+        <li><strong>Email:</strong>{{ friend.email }}</li> 
+    </ul>
+</li>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            detailsAreVisible: false,
+            friend: {
+                
+                    id: "Ava",
+                    name: "Ava Adams",
+                    phone: '111 111 1111',
+                    email: 'ava@test.com'
+            
+                    }
+        }
+    },
+    methods: {
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+};
+</script>
