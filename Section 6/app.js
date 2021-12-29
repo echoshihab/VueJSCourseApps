eb2e33ae403f3e2836650e25d353fa60f781a04f
echoshihab <echoshihab@gmail.com>
2021-12-29 11:38:50 -0500

    convert friend list demo app to use component

--- Section 6/app.js
+++ Section 6/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data(){
         return {
-            detailsAreVisible: false,
             friends: [
                 {
                     id: 'ava',
@@ -18,13 +17,38 @@ const app = Vue.createApp({
             ],
         };
     },
-    methods: {
-        toggleDetails(){
-           this.detailsAreVisible = !this.detailsAreVisible; 
-        }
-    }
+    
 
 });
 
+app.component('friend-contact', {
+    template: `
+    <li>
+          <h2>{{friend.name}}</h2>
+          <button @click="toggleDetails()">{{detailsAreVisible ? "Hide" : "Show"}} Details</button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{friend.phone}}</li>
+            <li><strong>Email:</strong> {{friend.email}}</li>
+          </ul>
+        </li>
+        <li>
+    `,
+    data() {
+    return { 
+        detailsAreVisible : false,
+        friend: {
+                    id: 'ava',
+                    name: 'Ava Adams',
+                    phone: '111 111 1111',
+                    email: 'ava@test.com'
+                }
+            };
+        },
+    methods: {
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible; 
+        }
+    }
+});
 
 app.mount('#app');
