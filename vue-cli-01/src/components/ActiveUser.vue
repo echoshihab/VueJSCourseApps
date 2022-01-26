d95321034709bcdb8218a08ef3554f6cdb46da3c
echoshihab <echoshihab@gmail.com>
2022-01-25 19:06:09 -0500

    add userdata and active user component

---
+++ vue-cli-01/src/components/ActiveUser.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <h1>Name: </h1>
+        <h1>Age: </h1>
+    </header>
+</template>
+
+<script>
+ export default {
+     props: {
+         name: {
+             type: String,
+             required: true
+         },
+         Age: {
+             type: String,
+             required: true
+         },
+     }
+ }
+</script>
---
+++ vue-cli-01/src/components/UserData.vue
@@ -0,0 +1,32 @@
+<template>
+<form @submit.prevent="submitData">
+    <div>
+        <label>Name: </label>
+        <input type="text" v-model="enteredName">
+    </div>
+    <div>
+        <label>Age: </label>
+        <input type="text" v-model="enteredAge">
+    </div>
+    <div>
+        <button>Add User Data</button>
+    </div>
+</form>
+</template>
+
+<script>
+export default {
+    emits: ['add-data'],
+    data(){
+        return {
+            enteredName: '',
+            enteredAge: '',
+        }
+    },
+    methods: {
+        submitData(){
+            this.$emit('add-data', this.enteredName, this.enteredAge);
+        }
+    }
+};
+</script>
