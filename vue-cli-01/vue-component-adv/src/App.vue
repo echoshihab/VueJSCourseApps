530ce37b67ddddcf14fde4ebc16ac92d0ee0a39a
echoshihab <echoshihab@gmail.com>
2022-02-15 20:57:39 -0500

    convert BadgeList, UserInfo, and TheHeader global component to local

--- vue-cli-01/vue-component-adv/src/App.vue
+++ vue-cli-01/vue-component-adv/src/App.vue
@@ -11,7 +11,16 @@
 </template>
 
 <script>
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+import TheHeader from './components/TheHeader.vue';
+
 export default {
+  components: {
+    'the-header': TheHeader,
+    BadgeList,
+    UserInfo
+    },
   data() {
     return {
       activeUser: {
