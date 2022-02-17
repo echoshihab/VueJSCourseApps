ba1d5a423c5f1bb828e67b412bc0ea000eca5bc8
echoshihab <echoshihab@gmail.com>
2022-02-17 09:22:27 -0500

    add scoped style to components and fix broken global style

--- vue-cli-01/vue-component-adv/src/App.vue
+++ vue-cli-01/vue-component-adv/src/App.vue
@@ -18,8 +18,8 @@ import TheHeader from './components/TheHeader.vue';
 export default {
   components: {
     'the-header': TheHeader,
-    BadgeList,
-    UserInfo
+    BadgeList, //this can be translated as badge-list or BadgeList
+    UserInfo 
     },
   data() {
     return {
