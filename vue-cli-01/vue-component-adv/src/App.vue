fa71545fc9db2568da6a43da0ef82fcdff5556c2
echoshihab <echoshihab@gmail.com>
2022-02-20 20:11:17 -0500

    add course goals component and add scoped slot for goals

--- vue-cli-01/vue-component-adv/src/App.vue
+++ vue-cli-01/vue-component-adv/src/App.vue
@@ -7,6 +7,11 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{slotProps.item}}</h2>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,15 +19,17 @@
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
 import TheHeader from './components/TheHeader.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   components: {
     'the-header': TheHeader,
     BadgeList, //this can be translated as badge-list or BadgeList
-    UserInfo 
+    UserInfo,
+    CourseGoals
     },
   data() {
-    return {
+  return {
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
