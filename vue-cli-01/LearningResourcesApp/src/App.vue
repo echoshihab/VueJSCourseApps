1f846cca44bf1425a25c5384bf327c25f220074e
echoshihab <echoshihab@gmail.com>
2022-02-21 18:35:11 -0500

    add initial files for learning resources app

---
+++ vue-cli-01/LearningResourcesApp/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<ul>
+</ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                { id: 'oficial-guide', 
+                title: 'Official Guide', 
+                description: 'The official Vue.js documentation',
+                link: 'https://vuejs.org'},
+                { id: 'google', 
+                title: 'Google', 
+                description: 'Learn to google...',
+                link: 'https://google.org'}
+            ]
+        };
+    }
+}
+</script>
