bc78e8ed419edde85254085c1ff7f5d76a7f90a5
echoshihab <echoshihab@gmail.com>
2022-02-21 19:04:23 -0500

    and learning resources component and loop through demo data

--- vue-cli-01/LearningResourcesApp/src/App.vue
+++ vue-cli-01/LearningResourcesApp/src/App.vue
@@ -1,21 +1,35 @@
 <template>
 <ul>
+    <learning-resource v-for="resource in storedResources" 
+    :key="resource.id" :title="resource.title" 
+    :description="resource.description"
+    :link="resource.link">
+    </learning-resource>
 </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
 export default {
+    components: {
+        LearningResource,
+    },
     data() {
         return {
             storedResources: [
-                { id: 'oficial-guide', 
+                { 
+                id: 'oficial-guide', 
                 title: 'Official Guide', 
                 description: 'The official Vue.js documentation',
-                link: 'https://vuejs.org'},
-                { id: 'google', 
+                link: 'https://vuejs.org'
+                },
+                {
+                id: 'google', 
                 title: 'Google', 
                 description: 'Learn to google...',
-                link: 'https://google.org'}
+                link: 'https://google.org'
+                }
             ]
         };
     }
