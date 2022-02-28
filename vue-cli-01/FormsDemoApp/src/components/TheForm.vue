0113abd01069d02ba5f305e25870b6305e01c7ab
echoshihab <echoshihab@gmail.com>
2022-02-28 09:04:04 -0500

    handle multiple checkboxes and radio buttons on submit

--- vue-cli-01/FormsDemoApp/src/components/TheForm.vue
+++ vue-cli-01/FormsDemoApp/src/components/TheForm.vue
@@ -19,30 +19,30 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input id="interest-news" name="interest" type="checkbox" value="news" v-model="interest"/>
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input id="interest-tutorials" name="interest" type="checkbox" value="tutorials" v-model="interest"/>
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input id="interest-nothing" name="interest" type="checkbox" value="nothing" v-model="interest"/>
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input id="how-video" name="how" value="video" type="radio" v-model="how"/>
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input id="how-blogs" name="how" value="blogs" type="radio" v-model="how"/>
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input id="how-other" name="how" value="other" type="radio" v-model="how"/>
         <label for="how-other">Other</label>
       </div>
     </div>
@@ -58,7 +58,9 @@ export default{
     return {
       userName: '',
       userAge: null,
-      referrer: 'wom'
+      referrer: 'wom',
+      interest: [],
+      how: null
 
     }
   },
@@ -67,9 +69,13 @@ export default{
       console.log(this.userName);
       console.log(this.userAge);
       console.log(this.referrer);
+      console.log(this.interest);
+      console.log(this.how);
       this.userName = '';
       this.userAge = null;
       this.referrer = 'wom';
+      this.interest = [];
+      this.how = null;
     }
   }
 }
