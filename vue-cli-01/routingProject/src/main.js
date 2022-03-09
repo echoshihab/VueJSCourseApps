3beea85646a4b0ce2ea9457489afa437dfef81c4
Shihab Khan <echoshihab@gmail.com>
2022-03-09 08:25:31 -0500

    add teamslist and userslist routes

--- vue-cli-01/routingProject/src/main.js
+++ vue-cli-01/routingProject/src/main.js
@@ -3,11 +3,18 @@ import {createRouter, createWebHistory} from 'vue-router';
 
 import App from './App.vue';
 
+const TeamsList = () => import('./components/teams/TeamsList.vue');
+const UsersList = () => import('./components/users/UsersList.vue');
+
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        { path: '/teams', component: TeamsList},
+        { path: '/users', component: UsersList} //our-domain.com/teams => ...
+    ]
 });
 
 const app = createApp(App)
+app.use(router);
 
 app.mount('#app');
