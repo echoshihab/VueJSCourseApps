4565c07e6215bb39e628660520f4091c1e6a22bc
Shihab Khan <echoshihab@gmail.com>
2022-03-13 17:20:42 -0400

    add redirect for domian root

--- vue-cli-01/routingProject/src/main.js
+++ vue-cli-01/routingProject/src/main.js
@@ -11,6 +11,7 @@ const TeamMembers = () => import('./components/teams/TeamMembers.vue');
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {path: '/', redirect: '/teams'},
         { path: '/teams', component: TeamsList},
         { path: '/users', component: UsersList},
         { path: '/teams/:teamId', component: TeamMembers, props: true},
