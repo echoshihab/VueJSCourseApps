00f27d26b96f13aac0b75c21fbae91e66b5cc640
Shihab Khan <echoshihab@gmail.com>
2022-03-17 09:31:28 -0400

    render footer views as secondary routes

--- vue-cli-01/routingProject/src/main.js
+++ vue-cli-01/routingProject/src/main.js
@@ -8,15 +8,23 @@ const TeamsList = () => import('./components/teams/TeamsList.vue');
 const UsersList = () => import('./components/users/UsersList.vue');
 const TeamMembers = () => import('./components/teams/TeamMembers.vue');
 const NotFound = () => import('./components/nav/NotFound.vue');
+const TeamsFooter = () => import('./components/teams/TeamsFooter.vue');
+const UsersFooter = () => import('./components/users/UsersFooter.vue');
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', redirect: '/teams'},
-        { name: 'teams', path: '/teams', component: TeamsList, children: [
+        { 
+            name: 'teams', 
+            path: '/teams', 
+            components: {default: TeamsList, footer: TeamsFooter},
+            children: [
          { name: 'team-members', path: ':teamId', component: TeamMembers, props: true},
         ]},
-        { path: '/users', component: UsersList},
+        { 
+            path: '/users', 
+            components: {default: UsersList, footer: UsersFooter}},
 
         { path: '/:notFound(.*)', component: NotFound}
          //our-domain.com/teams => ...
