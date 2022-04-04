b7a0318c3cda8b2a7690645b687cf04f4504315d
Shihab Khan <echoshihab@gmail.com>
2022-04-04 09:17:45 -0400

    add remove user, add user functionality and add styling

--- vue-cli-01/animationsDemo/src/components/UsersList.vue
+++ vue-cli-01/animationsDemo/src/components/UsersList.vue
@@ -1,7 +1,11 @@
 <template>
     <ul>
-        <li v-for="user in users" :key="user">{{ user }}</li>
+        <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
     </ul>
+    <div>
+        <input type="text" ref="userNameInput" />
+        <button @click="addUser">Add User</button>
+    </div>
 </template>
 
 <script>
@@ -10,6 +14,29 @@ export default {
         return {
             users: ['Max', 'Manu', 'Julie', 'Angela', 'Michael']
         };
+    },
+    methods: {
+        addUser(){
+            const enteredUserName = this.$refs.userNameInput.value;
+            this.users.unshift(enteredUserName);
+        },
+        removeUser(user){
+            this.users = this.users.filter(usr => usr !== user);
+        }
     }
 }
 </script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 1;
+    padding: 1;
+}
+
+li {
+    border: 1px solid #ccc;
+    padding: 1rem;
+    text-align: center;
+}
+</style>
