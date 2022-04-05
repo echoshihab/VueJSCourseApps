e2e822b262f6a991f05bd8a799a4652cb05d5b4a
Shihab Khan <echoshihab@gmail.com>
2022-04-05 10:17:56 -0400

    add animation for adding users

--- vue-cli-01/animationsDemo/src/components/UsersList.vue
+++ vue-cli-01/animationsDemo/src/components/UsersList.vue
@@ -1,7 +1,7 @@
 <template>
-    <ul>
-        <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
-    </ul>
+        <transition-group tag="ul" name="user-list">
+            <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
+        </transition-group>
     <div>
         <input type="text" ref="userNameInput" />
         <button @click="addUser">Add User</button>
@@ -28,6 +28,8 @@ export default {
 </script>
 
 <style scoped>
+
+
 ul {
     list-style: none;
     margin: 1;
@@ -38,5 +40,19 @@ li {
     border: 1px solid #ccc;
     padding: 1rem;
     text-align: center;
+}
+
+.user-list-enter-from {
+    opacity: 0;
+    transform: translateX(-30px);
+}
+
+.user-list-enter-active {
+    transition: all 1s ease-out;
+}
+
+.user-list-enter-to {
+    opacity: 1;
+    transform: translateX(0);
 }
 </style>
