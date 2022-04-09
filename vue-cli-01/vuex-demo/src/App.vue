483c5fdc2cd1cda520e959f0b0675f71c0b4de49
Shihab Khan <echoshihab@gmail.com>
2022-04-09 19:54:56 -0400

    change data through mutations

--- vue-cli-01/vuex-demo/src/App.vue
+++ vue-cli-01/vuex-demo/src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <base-container title="Vuex">
     <the-counter></the-counter>
+    <change-counter></change-counter>
     <h3>{{counter}}</h3>
-    <button @click="addOne">Add 1</button>
+    <button @click="addOne">Add 10</button>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
 
 export default {
   components: {
     BaseContainer,
-    TheCounter
-  },
-  computed:{
-    counter(){
-      return this.$store.state.counter;
-    }
+    TheCounter,
+    ChangeCounter,
   },
   methods:{
     addOne(){
-      this.$store.state.counter++;
+      this.$store.commit('increase', {value: 10});
     }
   },
   data() {
