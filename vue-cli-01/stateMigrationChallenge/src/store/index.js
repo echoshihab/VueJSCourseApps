e4b4e71a0a4010e432dab2e9becf9f4ee8ba55ba
Shihab Khan <echoshihab@gmail.com>
2022-04-22 11:43:55 -0400

    refactor cart to use store - work in progress

--- vue-cli-01/stateMigrationChallenge/src/store/index.js
+++ vue-cli-01/stateMigrationChallenge/src/store/index.js
@@ -1,7 +1,13 @@
 import { createStore } from 'vuex';
 
+import productsModule from './modules/product.js';
+import cartModule from './modules/cart.js';
+
 const store = createStore({
-    
+    modules: {
+        prods: productsModule,
+        cart: cartModule
+    }
 });
 
 export default store;
