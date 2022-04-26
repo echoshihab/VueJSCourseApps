ca6f27929eed2375ec7fa8dae3ed07d3b1781dff
Shihab Khan <echoshihab@gmail.com>
2022-04-26 08:53:59 -0400

    add header component

--- vue-cli-01/find_a_coach/src/router.js
+++ vue-cli-01/find_a_coach/src/router.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import RequestsReceived from './pages/requests/RequestReceived.vue';
 import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
-  routers: [
+  routes: [
     { path: '/', redirect: '/coaches' },
     { path: '/coaches', component: CoachesList },
     {
