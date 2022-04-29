6069c21062908c9fccdfada82faed2ba79294b46
Shihab Khan <echoshihab@gmail.com>
2022-04-29 09:41:34 -0400

    add coaches item component and use in coaches list component

--- vue-cli-01/find_a_coach/src/pages/coaches/CoachesList.vue
+++ vue-cli-01/find_a_coach/src/pages/coaches/CoachesList.vue
@@ -7,16 +7,28 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      >
+      </coach-item>
     </ul>
     <h3 v-else>No coaches Found</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -27,3 +39,15 @@ export default {
   },
 };
 </script>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
