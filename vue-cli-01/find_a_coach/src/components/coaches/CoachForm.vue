e454732df8276158be7d305081f2b06ef43f6b25
Shihab Khan <echoshihab@gmail.com>
2022-05-06 16:27:37 -0400

    add ability to add to coach to store data via form

--- vue-cli-01/find_a_coach/src/components/coaches/CoachForm.vue
+++ vue-cli-01/find_a_coach/src/components/coaches/CoachForm.vue
@@ -37,6 +37,7 @@
 
 <script>
 export default {
+  emits: ['save-data'],
   data() {
     return {
       firstname: '',
@@ -56,7 +57,7 @@ export default {
         areas: this.areas,
       };
 
-      console.log(formData);
+      this.$emit('save-data', formData);
     },
   },
 };
