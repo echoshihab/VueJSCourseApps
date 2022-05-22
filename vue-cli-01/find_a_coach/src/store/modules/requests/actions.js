8768e285bb3cf2a78c6c914f6a8fe562ad81d916
Shihab Khan <echoshihab@gmail.com>
2022-05-22 19:29:24 -0400

    persist requests for coach in firebase

--- vue-cli-01/find_a_coach/src/store/modules/requests/actions.js
+++ vue-cli-01/find_a_coach/src/store/modules/requests/actions.js
@@ -1,11 +1,27 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+    const response = await fetch(
+      `${process.env.VUE_APP_FIREBASE_ADDRESS}requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to send');
+      throw error;
+    }
+
+    const responseData = await response.json();
+
+    newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
+
     context.commit('addRequest', newRequest);
   },
 };
