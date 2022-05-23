66efaad5a36ad2b7788ff6d362c29b1b237daed9
Shihab Khan <echoshihab@gmail.com>
2022-05-23 10:30:14 -0400

    fetch request data from firebase

--- vue-cli-01/find_a_coach/src/store/modules/requests/actions.js
+++ vue-cli-01/find_a_coach/src/store/modules/requests/actions.js
@@ -24,4 +24,32 @@ export default {
 
     context.commit('addRequest', newRequest);
   },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const response = await fetch(
+      `${process.env.VUE_APP_FIREBASE_ADDRESS}requests/${coachId}.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to fetch request'
+      );
+      throw error;
+    }
+
+    const requests = [];
+
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message,
+      };
+      requests.push(request);
+    }
+
+    context.commit('setRequests', requests);
+  },
 };
