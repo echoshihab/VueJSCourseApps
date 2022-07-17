37ea5ebee4979a9c643b9c32f2bfbc343e47d4d3
Shihab Khan <echoshihab@gmail.com>
2022-07-17 19:34:16 -0400

    add login action and flow

--- vue-cli-01/find_a_coach/src/store/modules/auth/actions.js
+++ vue-cli-01/find_a_coach/src/store/modules/auth/actions.js
@@ -1,5 +1,35 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_API_KEY}`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to authenticate. Check your login data.'
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+
   async signup(context, payload) {
     const response = await fetch(
       `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_API_KEY}`,
@@ -18,7 +48,8 @@ export default {
     if (!response.ok) {
       console.log(responseData);
       const error = new Error(
-        responseData.message || 'Failed to authenticate. Check your login data.'
+        responseData.message ||
+          'Failed to authenticate. Check your signup data.'
       );
       throw error;
     }
